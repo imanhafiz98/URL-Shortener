024580fa4d664cbfa691db806d9d7f2adf164066
runningman1 <71074599+runningman1@users.noreply.github.com>
2021-04-07 10:22:16 +0800

    Initial Commit

---
+++ database/migrations/2021_04_07_020430_create_short_links_table.php
@@ -0,0 +1,34 @@
+<?php
+  
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+   
+class CreateShortLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code');
+            $table->string('link');
+            $table->integer('visits')->default(0);
+            $table->timestamps();
+        });
+    }
+   
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_links');
+    }
+}
