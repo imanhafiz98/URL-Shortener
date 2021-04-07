fb2e3138d5e6f2990aab0287ceab2625a2e31d02
runningman1 <71074599+runningman1@users.noreply.github.com>
2021-04-07 10:32:36 +0800

    Add ShortLinkController

---
+++ app/Http/Controllers/ShortLinkController.php
@@ -0,0 +1,85 @@
+<?php
+  
+namespace App\Http\Controllers;
+   
+use Illuminate\Http\Request;
+use App\Models\ShortLink;
+use Str;
+
+class ShortLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $shortLinks = ShortLink::latest()->get();
+   
+        return view('shortenLink', compact('shortLinks'));
+    }
+     
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'link' => 'required|url']);
+        }
+
+           if($request->has('checkauto'))
+           {
+                $request->validate([
+                    'link' => 'required|url']);
+
+                 $input['link'] = $request->link;
+                 $input['code'] = Str::random(6);
+           }
+
+           else
+           {
+                 $request->validate([
+                     'link' => 'required|url',
+                     'code' => 'required|unique:short_links']);
+
+                $input['link'] = $request->link;
+                $input['code'] = $request->code;
+           }
+
+        ShortLink::create($input);
+  
+        return redirect('generate-shorten-link')
+             ->with('success', 'Shorten Link Generated Successfully!');
+             
+    }
+   
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     
+    public function shortenLink(ShortLink $shortlink)
+    {
+        $shortlink->visits= $shortlink->visits + 1;
+        $shortlink->save();
+
+        return redirect($shortlink->link);
+    }
+
+    
+    public function destroy(ShortLink $shortlink)
+    {
+        $shortlink->delete();
+
+        return redirect('generate-shorten-link')
+            ->with('success','Link deleted successfully');
+
+    }
+    
+}
